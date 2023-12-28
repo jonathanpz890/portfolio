432d7ab26228c72185a1aeca0828329cc682001c
Jonathanpz890 <Jonathanpz890@gmail.com>
2023-12-28 13:33:51 +0200

    firebase

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,31 @@ import { Navbar } from './components/Navbar/Navbar';
 import { Projects } from './components/Projects';
 import { Box } from '@mui/material';
 import { Footer } from './components/Footer/Footer';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const App = () => {
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+
+    // Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    const firebaseConfig = {
+        apiKey: "AIzaSyB1s4AjlQmei6XXU5E_-MLAhAReP50CJls",
+        authDomain: "test-43d00.firebaseapp.com",
+        projectId: "test-43d00",
+        storageBucket: "test-43d00.appspot.com",
+        messagingSenderId: "976269096203",
+        appId: "1:976269096203:web:55802c55fc591734db1e0e",
+        measurementId: "G-TDWNJ1BT6Y"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+    
     const [deformIntro, setDeformIntro] = useState(false)
     const [experienceAnimate, setExperienceAnimate] = useState(false)
 
